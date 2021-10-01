b37816bbecd26df72f87618e741ec8ef970c9a89
tiko34 <tixonkov29@gmail.com>
2021-10-01 21:09:23 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Linq;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace evg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lst.ItemsSource = bdagent.GetContext().Agent.ToList();
+            //SqlCommand sc = new SqlCommand(ToString());
+          //  sc.CommandText = "SELECT*FROM AgentTypeID";
+            
+        }
+    }
+}
